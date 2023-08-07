0270c7549652337636fff0b4e9b2a7c7e1094941
Stefan <118810289+StefanEpic@users.noreply.github.com>
2023-08-07 17:22:23 +0300

    Update repository.py

--- src/utils/repository.py
+++ src/utils/repository.py
@@ -17,39 +17,39 @@ class SQLAlchemyRepository(AbstractRepository):
 
     async def get_list(self):
         stmt = select(self.model)
-        res = await self.session.execute(stmt)
-        res = [row[0] for row in res.all()]
+        res = await self.session.execute(stmt).all()
         return res
 
-
-    # async def select_one(self, **filter_by):
-    #     async with async_session_maker() as session:
-    #         stmt = select(self.model).filter_by(**filter_by)
-    #         res = await session.execute(stmt)
-    #         res = res.one_or_none()
-    #         if res:
-    #             res = res[0].to_read_model()
-    #         return {"status": 200, "detail": res}
-    #
-    # async def add_one(self, data) -> int:
-    #     async with async_session_maker() as session:
-    #         session.add(data)
-    #         await session.commit()
-    #         await session.refresh(data)
-    #         return {"status": 200, "detail": data.id}
-    #
-    # async def edit_one(self, data: dict, **filter_by):
-    #     async with async_session_maker() as session:
-    #         stmt = update(self.model).filter_by(**filter_by).values(**data).returning(self.model.id)
-    #         res = await session.execute(stmt)
-    #         await session.commit()
-    #         res = res.scalar_one()
-    #         return {"status": 200, "detail": res}
-    #
-    # async def delete_oneself, **filter_by) -> int:
-    #     async with async_session_maker() as session:
-    #         stmt = delete(self.model).filter_by(**filter_by).returning(self.model.id)
-    #         res = await session.execute(stmt)
-    #         await session.delete()
-    #         await session.commit()
-    #         return {"status": 200, "detail": res.scalar_one()}
+    async def select_one(self, self_id: int):
+        res = await self.session.get(self.model, self_id)
+        if not res:
+            raise HTTPException(status_code=404, detail="Not found")
+        return res
+    
+    async def add_one(self, data):
+        res = self.model.from_orm(data)
+        self.session.add(res)
+        await self.session.commit()
+        await self.session.refresh(res)
+        return res
+    
+    async def edit_one(self, self_id: int, data):
+        res = await self.session.get(self_id, data)
+        if not res:
+            raise HTTPException(status_code=404, detail="Not found")
+        res_data = self.model.dict(exclude_unset=True)
+        for key, value in res_data.items():
+            setattr(res, key, value)
+        self.session.add(res)
+        await self.session.commit()
+        await self.session.refresh(res)
+        return res
+    
+    async def delete_one(self, self_id: int):
+        res = await self.session.get(self.model, self_id)
+        if not res:
+            raise HTTPException(status_code=404, detail="Not found")
+        self.session.delete(res)
+        await self.session.commit()
+        return {"ok": True}
+        
