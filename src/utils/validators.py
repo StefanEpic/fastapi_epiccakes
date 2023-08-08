4a70cdfe006c1b2c02c46e42baa5ebf8e33840c4
Stefan <118810289+StefanEpic@users.noreply.github.com>
2023-08-08 15:50:36 +0300

    Update validators.py

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -1,30 +1,30 @@
 from string import digits, punctuation, ascii_letters
 
 
-def get_str(n):
-    return [k for k,v in locals().iteritems() if v == n][0]
+def var2str(var, vars_data = locals()):
+    return [var_name for var_name in vars_data if id(var) == id(vars_data[var_name])][0]
 
 
 def name_valid(self, key, name):
     if digits in name or punctuation in name:
-        raise ValueError(f"invalid value for {get_str(name)} field")
+        raise ValueError(f"invalid value for {var2str(name)} field")
     return name
 
 
 def email_valid(self, key, email):
     if "@" not in email:
-        raise ValueError(f"invalid value for {get_str(email)} field")
+        raise ValueError(f"invalid value for {var2str(email)} field")
     return email
 
 
 def phone_valid(self, key, phone):
     punctuation = punctuation.replace('-', '') 
     if ascii_letters in phone or punctuation in phone:
-        raise ValueError(f"invalid value for {get_str(phone)} field")
+        raise ValueError(f"invalid value for {var2str(phone)} field")
     return phone
 
 
 def rating_valid(self, key, rating):
     if rating < 0 or rating > 5:
-        raise ValueError(f"invalid value for {get_str(rating)} field")
+        raise ValueError(f"invalid value for {var2str(rating)} field")
     return email
