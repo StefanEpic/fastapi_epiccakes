b7b807771560514888877ce154d84a810eccde5f
Stefan <118810289+StefanEpic@users.noreply.github.com>
2023-08-09 10:39:22 +0300

    Update manufacturer_manager.py

--- src/api/routers_store/manufacturer_manager.py
+++ src/api/routers_store/manufacturer_manager.py
@@ -1,40 +1,40 @@
-# from typing import List
-#
-# from fastapi import APIRouter, Depends
-# from sqlmodel import Session
-#
-# from db.db import get_session
-# from models.store.manufacturer_manager import ManufacturerManagerRead, ManufacturerManagerCreate, \
-#     ManufacturerManagerUpdate
-# from repositories.store import ManufacturerManagerRepository
-#
-# router = APIRouter(
-#     prefix="/manufacturer_managers",
-#     tags=["Manufacturer Managers"],
-# )
-#
-#
-# @router.get('', response_model=List[ManufacturerManagerRead])
-# async def get_manufacturers(session: Session = Depends(get_session)):
-#     return await ManufacturerManagerRepository(session).get_list()
-#
-#
-# @router.get('/{manager_id}', response_model=ManufacturerManagerRead)
-# async def get_manufacturer(manufacturer_id: int, session: Session = Depends(get_session)):
-#     return await ManufacturerManagerRepository(session).get_one(manufacturer_id)
-#
-#
-# @router.post('', response_model=ManufacturerManagerRead)
-# async def add_manufacturer(manufacturer: ManufacturerManagerCreate, session: Session = Depends(get_session)):
-#     return await ManufacturerManagerRepository(session).add_one(manufacturer)
-#
-#
-# @router.patch('/{manager_id}', response_model=ManufacturerManagerRead)
-# async def patch_manufacturer(manufacturer_id: int, manufacturer: ManufacturerManagerUpdate,
-#                              session: Session = Depends(get_session)):
-#     return await ManufacturerManagerRepository(session).edit_one(manufacturer_id, manufacturer)
-#
-#
-# @router.delete('/{manager_id}')
-# async def patch_manufacturer(manufacturer_id: int, session: Session = Depends(get_session)):
-#     return await ManufacturerManagerRepository(session).delete_one(manufacturer_id)
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+
+from db.db import get_session
+from models.store.manufacturer_manager import ManufacturerManagerRead, ManufacturerManagerCreate, \
+    ManufacturerManagerUpdate
+from repositories.store import ManufacturerManagerRepository
+
+router = APIRouter(
+    prefix="/manufacturer_managers",
+    tags=["Manufacturer Managers"],
+)
+
+
+@router.get('', response_model=List[ManufacturerManagerRead])
+async def get_list(session: Session = Depends(get_session)):
+    return await ManufacturerManagerRepository(session).get_list()
+
+
+@router.get('/{manager_id}', response_model=ManufacturerManagerRead)
+async def get_one(manager_id: int, session: Session = Depends(get_session)):
+    return await ManufacturerManagerRepository(session).get_one(manager_id)
+
+
+@router.post('', response_model=ManufacturerManagerRead)
+async def add_one(manager: ManufacturerManagerCreate, session: Session = Depends(get_session)):
+    return await ManufacturerManagerRepository(session).add_one(manager)
+
+
+@router.patch('/{manager_id}', response_model=ManufacturerManagerRead)
+async def edit_one(manager_id: int, manager: ManufacturerManagerUpdate,
+                             session: Session = Depends(get_session)):
+    return await ManufacturerManagerRepository(session).edit_one(manager_id, manager)
+
+
+@router.delete('/{manager_id}')
+async def delete_one(manager_id: int, session: Session = Depends(get_session)):
+    return await ManufacturerManagerRepository(session).delete_one(manager_id)
