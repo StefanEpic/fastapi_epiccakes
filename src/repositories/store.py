22303027650338ad3babc55121132b7bcaf9a93b
Stefan <118810289+StefanEpic@users.noreply.github.com>
2023-08-09 10:03:23 +0300

    Update store.py

--- src/repositories/store.py
+++ src/repositories/store.py
@@ -1,13 +1,4 @@
-from models.store.category import Category
-from models.store.client import Client
-from models.store.client_manager import ClientManager
-from models.store.image import Image
-from models.store.manufacturer import Manufacturer
-from models.store.manufacturer_manager import ManufacturerManager
-from models.store.order import Order
-from models.store.product import Product
-from models.store.review import Review
-
+from models.store import Category, Product, Image, Client, ClientManager, Manufacturer, ManufacturerManager, Order, Review
 from utils.repository import SQLAlchemyRepository
 
 
