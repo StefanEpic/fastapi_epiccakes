13fce68ce429ceca4676c7f618766240860344d0
Stefan <118810289+StefanEpic@users.noreply.github.com>
2023-08-09 10:01:29 +0300

    Delete src/models/store directory

--- src/models/store/category_product_link.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from sqlmodel import SQLModel, Field
-
-
-class CategoryProductLink(SQLModel, table=True):
-    category_id: Optional[int] = Field(default=None, foreign_key="category.id", primary_key=True)
-    product_id: Optional[int] = Field(default=None, foreign_key="product.id", primary_key=True)
--- src/models/store/order_product_link.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from sqlmodel import SQLModel, Field
-
-
-class OrderProductLink(SQLModel, table=True):
-    order_id: Optional[int] = Field(default=None, foreign_key="order.id", primary_key=True)
-    product_id: Optional[int] = Field(default=None, foreign_key="product.id", primary_key=True)
-    amount: int
