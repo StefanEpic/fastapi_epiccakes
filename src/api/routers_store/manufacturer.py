0bd879ae7eb0c0c48cceedcdb446d61adc4c149a
Stefan <118810289+StefanEpic@users.noreply.github.com>
2023-08-09 10:40:35 +0300

    Update manufacturer.py

--- src/api/routers_store/manufacturer.py
+++ src/api/routers_store/manufacturer.py
@@ -14,26 +14,26 @@ router = APIRouter(
 
 
 @router.get('', response_model=List[ManufacturerRead])
-async def get_manufacturers(session: Session = Depends(get_session)):
+async def get_list(session: Session = Depends(get_session)):
     return await ManufacturerRepository(session).get_list()
 
 
 @router.get('/{manufacturer_id}', response_model=ManufacturerRead)
-async def get_manufacturer(manufacturer_id: int, session: Session = Depends(get_session)):
+async def get_one(manufacturer_id: int, session: Session = Depends(get_session)):
     return await ManufacturerRepository(session).get_one(manufacturer_id)
 
 
 @router.post('', response_model=ManufacturerRead)
-async def add_manufacturer(manufacturer: ManufacturerCreate, session: Session = Depends(get_session)):
+async def add_one(manufacturer: ManufacturerCreate, session: Session = Depends(get_session)):
     return await ManufacturerRepository(session).add_one(manufacturer)
 
 
 @router.patch('/{manufacturer_id}', response_model=ManufacturerRead)
-async def patch_manufacturer(manufacturer_id: int, manufacturer: ManufacturerUpdate,
+async def edit_one(manufacturer_id: int, manufacturer: ManufacturerUpdate,
                              session: Session = Depends(get_session)):
     return await ManufacturerRepository(session).edit_one(manufacturer_id, manufacturer)
 
 
 @router.delete('/{manufacturer_id}')
-async def patch_manufacturer(manufacturer_id: int, session: Session = Depends(get_session)):
+async def delete_one(manufacturer_id: int, session: Session = Depends(get_session)):
     return await ManufacturerRepository(session).delete_one(manufacturer_id)
